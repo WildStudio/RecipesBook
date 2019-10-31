3407e3231979967e7f8aa7bc79ee315028d741ec
WildStudio <christian.aranda@gmail.com>
2019-10-31 12:05:01 +0100

    Add app dependencies and Recipes repository

---
+++ RecipesBook/Repositories/RecipesRepository.swift
@@ -0,0 +1,20 @@
+//
+//  RecipesRepository.swift
+//  RecipesBook
+//
+//  Created by wearemobilefirst on 31/10/2019.
+//  Copyright © 2019 badi. All rights reserved.
+//
+
+import Foundation
+import RecipePuppyKit
+
+struct RecipesRepository {
+    
+    private let service: ServiceType
+
+    init(service: ServiceType) {
+        self.service = service
+    }
+    
+}
