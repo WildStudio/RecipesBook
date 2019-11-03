66d73f5a337e0079121afcd5ef1f5e6b6bf90e3a
WildStudio <christian.aranda@gmail.com>
2019-11-03 16:46:46 +0100

    delete unused view controller

--- RecipePuppy/SceneDelegate.swift
+++ RecipePuppy/SceneDelegate.swift
@@ -10,9 +10,9 @@ import UIKit
 import RecipePuppyKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     private lazy var appCoordinator: AppCoordinator = {
         return AppCoordinator(dependencyProvider: dependencyProvider)
     }()
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     private lazy var dependencyProvider: DependencyProvider = {
         return makeDependencyProvider(service: Service(serverConfig: ServerConfig(apiBaseUrl: URL(string: "http://www.recipepuppy.com/")!)))
     }()
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
         guard let navigationController = window?.rootViewController as? UINavigationController
@@ -30,29 +30,29 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         appCoordinator.setup(for: navigationController)
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
@@ -60,10 +60,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     private func makeDependencyProvider(service: ServiceType) -> DependencyProvider {
-           let repositoryProvider = RepositoryProvider(service: service)
-           return DependencyProvider(provider: repositoryProvider, recipePuppyService: service)
+        let repositoryProvider = RepositoryProvider(service: service)
+        return DependencyProvider(provider: repositoryProvider, recipePuppyService: service)
     }
-
-
+    
 }
 
